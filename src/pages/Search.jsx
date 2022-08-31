ae9530d8f12040827ff1fecfefbfcd49d193b856
IsraelPereira <pereira.israel2070@gmail.com>
2022-08-31 16:34:20 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,39 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchField: '',
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      searchField: value,
+    });
+  };
+
   render() {
+    const minSearchLenght = 2;
+    const { searchField } = this.state;
     return (
       <div>
         <Header />
         <div data-testid="page-search">
-          Search
+          <form>
+            <input
+              data-testid="search-artist-input"
+              name="searchField"
+              placeholder="Nome do Artísta"
+              onChange={ this.handleChange }
+              type="text"
+            />
+            <button
+              disabled={ searchField.length < minSearchLenght }
+              data-testid="search-artist-button"
+              type="button"
+            >
+              Pesquisar
+            </button>
+          </form>
         </div>
       </div>
     );
