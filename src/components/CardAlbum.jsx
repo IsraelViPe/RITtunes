c3b360d918971d5921d230492c21dbbf24b2b137
IsraelPereira <pereira.israel2070@gmail.com>
2022-08-31 21:00:12 -0300

    requisito 6

---
+++ src/components/CardAlbum.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+export default class CardAlbum extends Component {
+  render() {
+    const { collectionName, artistName, artworkUrl100, collectionId } = this.props;
+    return (
+      <div>
+        <div>
+          <img src={ artworkUrl100 } alt={ artistName } />
+        </div>
+        <span>{ collectionName }</span>
+        <span>{ artistName }</span>
+        <Link
+          data-testid={ `link-to-album-${collectionId}` }
+          to={ `album/${collectionId}` }
+        />
+      </div>
+    );
+  }
+}
+
+CardAlbum.propTypes = {
+  collectionName: PropTypes.string.isRequired,
+  artistName: PropTypes.string.isRequired,
+  artworkUrl100: PropTypes.string.isRequired,
+  collectionId: PropTypes.string.isRequired,
+};
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import CardAlbum from '../components/CardAlbum';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
   state = {
     searchField: '',
+    artistSearched: '',
+    responseAPI: false,
+    statusAPI: false,
+    fetchClicked: false,
+    listAlbuns: [],
   };
 
   handleChange = ({ target: { value } }) => {
@@ -12,29 +20,78 @@ export default class Search extends Component {
     });
   };
 
+  handleClickSearch = async () => {
+    const { searchField } = this.state;
+    this.setState({
+      artistSearched: searchField,
+      searchField: '',
+      fetchClicked: true,
+      responseAPI: false,
+    });
+    const fetchAlbuns = await searchAlbumsAPI(searchField);
+    this.setState({
+      fetchClicked: false,
+      responseAPI: true,
+      statusAPI: fetchAlbuns.length !== 0,
+      listAlbuns: await fetchAlbuns,
+    });
+    console.log(fetchAlbuns);
+  };
+
   render() {
     const minSearchLenght = 2;
-    const { searchField } = this.state;
+    const { artistSearched, searchField, listAlbuns,
+      fetchClicked, responseAPI, statusAPI } = this.state;
+
+    const noAlbuns = (
+      <h1>Nenhum álbum foi encontrado</h1>
+    );
+
+    const albunsRender = (
+      <div>
+        <h1>
+          Resultado de álbuns de:
+          {' '}
+          { artistSearched }
+        </h1>
+        { listAlbuns
+          .map(({ collectionName, artistName, artworkUrl100, collectionId }) => (
+            <CardAlbum
+              key={ collectionId }
+              collectionName={ collectionName }
+              artistName={ artistName }
+              artworkUrl100={ artworkUrl100 }
+              collectionId={ collectionId }
+            />))}
+      </div>
+    );
+    const formSearch = (
+      <form>
+        <input
+          data-testid="search-artist-input"
+          value={ searchField }
+          name="searchField"
+          placeholder="Nome do Artísta"
+          onChange={ this.handleChange }
+          type="text"
+        />
+        <button
+          onClick={ this.handleClickSearch }
+          disabled={ searchField.length < minSearchLenght }
+          data-testid="search-artist-button"
+          type="button"
+        >
+          Pesquisar
+        </button>
+      </form>
+    );
     return (
       <div>
         <Header />
+
         <div data-testid="page-search">
-          <form>
-            <input
-              data-testid="search-artist-input"
-              name="searchField"
-              placeholder="Nome do Artísta"
-              onChange={ this.handleChange }
-              type="text"
-            />
-            <button
-              disabled={ searchField.length < minSearchLenght }
-              data-testid="search-artist-button"
-              type="button"
-            >
-              Pesquisar
-            </button>
-          </form>
+          {fetchClicked ? <Loading /> : formSearch }
+          {responseAPI && statusAPI ? albunsRender : noAlbuns }
         </div>
       </div>
     );
